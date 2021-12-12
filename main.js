911b444e358a055a2f01bc52ff68f5132f079c89
No Loss Club <95616694+nolossclub@users.noreply.github.com>
2021-12-12 05:23:08 +0400

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var text2png = require('text2png');
 const dir = './dist/';
 
 for (let index = 0; index < 10; index++) {
-	fs.writeFileSync(`${dir + index}.png`, text2png(`NOLOSSCLUB \nTicket \n#${index}`, {  font: '80px Futura', color: 'teal',  lineSpacing: 25, padding: 50}));
+	fs.writeFileSync(`${dir + index}.png`, text2png(`NO LOSS CLUB \nTicket \n#${index}`, {  font: '80px Futura', color: 'teal',  lineSpacing: 80, padding: 50}));
 
-	fs.writeFileSync(`${dir + index}`, text2png(`NOLOSSCLUB \nTicket \n#${index}`, {  font: '80px Futura', color: 'teal',  lineSpacing: 25, padding: 50}));
+	fs.writeFileSync(`${dir + index}`, text2png(`NO LOSS CLUB \nTicket \n#${index}`, {  font: '80px Futura', color: 'teal',  lineSpacing: 80, padding: 50}));
 }
